59c45d021d6d7a1faabe39ce847baf74f210453d
Xhanti Mzozoyana <xhantimzozoyana123@gmail.com>
2026-06-16 20:35:08 +0200

    Added //

--- TubeMailGorilla.Domain/Interfaces/IEmailerRepository.cs
+++ TubeMailGorilla.Domain/Interfaces/IEmailerRepository.cs
@@ -4,6 +4,7 @@ namespace TubeMailGorillaDomain.Interfaces
 {
     public interface IEmailerRepository
     {
+        //
         IQueryable<Emailer> GetAll();
         Task<List<Emailer>> GetAllAsync();
         Task<Emailer?> GetByIdAsync(int id);
